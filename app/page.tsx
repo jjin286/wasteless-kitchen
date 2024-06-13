25fe5dbbc961b6af40657d1794a2ec2046f59910
JaeWon Jin <j1jin286@gmail.com>
2024-06-12 17:45:13 -0700

    Created initial Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '../components/HomeHeroSection';
 import BenefitSection from '../components/HomeBenefitSection';
 import FeatureSection from '../components/HomeFeatureSection';
+import Nav from '../components/Nav';
 
 export default function Home() {
   return (
     <div className='home'>
+        <Nav />
         <HeroSection text='Welcome to Wasteless Kitchen!'/>
       <div className='info'>
 
---
+++ components/Nav.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import { useState } from "react";
+
+export default function Nav(){
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleNav = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    return(
+        <nav className="fixed w-full h-24 shadow-xl bg-white">
+            <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
+                <Link href={'/'}>
+                    <Image
+                        src={'next.svg'}
+                        alt=""
+                        width="200"
+                        height="75"
+                        className=""
+                        priority
+                    />
+                </Link>
+                <div className="hidden sm:flex">
+                    <ul className="hidden sm:flex">
+                        <Link href={''}>
+                            <li className="ml-10 uppercase hover:border-b text-xl">
+                                Recipe
+                            </li>
+                        </Link>
+                        <Link href={''}>
+                            <li className="ml-10 uppercase hover:border-b text-xl">
+                                Ingredient
+                            </li>
+                        </Link>
+                        <Link href={''}>
+                            <li className="ml-10 uppercase hover:border-b text-xl">
+                                Search
+                            </li>
+                        </Link>
+                        <Link href={''}>
+                            <li className="ml-10 uppercase hover:border-b text-xl">
+
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
+                    <AiOutlineMenu size={25} />
+                </div>
+            </div>
+            <div className={
+                menuOpen
+                ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+            }>
+                <div className="flex w-full items-center justify-end">
+                    <div onClick={handleNav} className="cursor-pointer">
+                        <AiOutlineClose size={25} />
+                    </div>
+                </div>
+                <div className="flex-col py-4 cursor-pointer">
+                    <ul>
+                        <Link href={"/"}>
+                            <li
+                                onClick={() => setMenuOpen(false)}
+                                className="py-4 cursor-pointer"
+                            >
+                                Recipe
+                            </li>
+                        </Link>
+                        <Link href={"/"}>
+                            <li
+                                onClick={() => setMenuOpen(false)}
+                                className="py-4 cursor-pointer"
+                            >
+                                Ingredient
+                            </li>
+                        </Link>
+                        <Link href={"/"}>
+                            <li
+                                onClick={() => setMenuOpen(false)}
+                                className="py-4 cursor-pointer"
+                            >
+                                Search
+                            </li>
+                        </Link>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
