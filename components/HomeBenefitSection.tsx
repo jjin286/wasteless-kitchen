1699f7020cd42adc736287fab666891f72c68c7d
JaeWon Jin <j1jin286@gmail.com>
2024-06-14 11:52:29 -0700

    Add icons for feature section

--- components/HomeBenefitSection.tsx
+++ components/HomeBenefitSection.tsx
@@ -2,10 +2,16 @@ import Card from "./Card";
 
 export default function BenefitSection(){
     return(
-        <div className="benefits flex justify-evenly items-center text-center h-[80vh]">
-            <Card image="/next.svg" text="Reduce food waste"/>
-            <Card image="/next.svg" text="Save money"/>
-            <Card image="/next.svg" text="Try new recipes"/>
+        <div className="benefits text-center content-center h-[80vh] pb-40">
+            <div className="h-1/2 content-center">
+                <h3>Become a wasteless cook using Wasteless Kitchen!</h3>
+            </div>
+            <div className="flex justify-evenly h-1/2 ">
+                <Card image="/recycle.png" text="Reduce food waste"/>
+                <Card image="/money.png" text="Save money"/>
+                <Card image="/cook-book.png" text="Try new recipes"/>
+            </div>
+
         </div>
     );
 }
