b9357954f1245c4f2ef2bb070cf5c6a00a7f4eb5
JaeWon Jin <j1jin286@gmail.com>
2024-06-27 19:40:09 -0700

    Added sortable select options that update search params

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,37 +1,51 @@
 'use client';
-import { useState } from "react";
+
+import * as React from "react"
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from 'use-debounce';
+import { Input } from "@/components/ui/input";
+import SortBySelect from "./SortBySelect";
+import { useState } from "react";
 
 export default function SearchBar(){
-    // const [searchTerm, setSearchTerm] = useState("");
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
+    const [sortBy, setSortBy] = useState("");
+    const params = new URLSearchParams(searchParams);
 
     const handleSearch = useDebouncedCallback((term) => {
-        const params = new URLSearchParams(searchParams);
         if (term) {
             params.set('query', term);
         } else {
             params.delete('query');
         }
+
         replace(`${pathname}?${params.toString()}`);
-        // setSearchTerm(term);
+    }, 300);
+
+    const handleSort = useDebouncedCallback((value:string) => {
+        if(value) {
+            params.set('sort', value);
+        } else {
+            params.delete('sort');
+        }
+
+        replace(`${pathname}?${params.toString()}`);
     }, 300);
 
     return(
         <div className="flex justify-center pt-12">
-            <input
+            <Input
                 type="text"
                 placeholder="Search"
                 className="p-3 w-3/4"
-                onChange={(e) => {handleSearch(e.target.value)}}
                 defaultValue={searchParams.get('query')?.toString()}
-                // value={searchTerm}
-
+                onChange={(e) => {
+                    handleSearch(e.target.value);
+                }}
             />
-
+            <SortBySelect handleSort={handleSort}/>
         </div>
     );
 }
---
+++ app/components/SortBySelect.tsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectGroup,
+    SelectLabel,
+  } from "@/components/ui/select";
+import { useSearchParams } from "next/navigation";
+
+const SORTABLE = ["(empty)","meta-score","popularity","healthiness","price","time","random",
+"max-used-ingredients","min-missing-ingredients","alcohol","caffeine","copper","energy","calories","calcium","carbohydrates",
+"carbs","choline","cholesterol","total-fat","fluoride","trans-fat","saturated-fat","mono-unsaturated-fat",
+"poly-unsaturated-fat","fiber","folate","folic-acid","iodine","iron","magnesium","manganese","vitamin-b3",
+"niacin","vitamin-b5","pantothenic-acid","phosphorus","potassium","protein","vitamin-b2","riboflavin","selenium",
+"sodium","vitamin-b1","thiamin","vitamin-a","vitamin-b6","vitamin-b12","vitamin-c","vitamin-d","vitamin-e",
+"vitamin-k","sugar","zinc"
+]
+
+export default function SortBySelect({handleSort}){
+    const searchParams = useSearchParams();
+    const sortable = SORTABLE.map((sortable) => {
+        return(
+            <SelectItem value={`${sortable}`}>{sortable.charAt(0).toUpperCase() + sortable.slice(1)}</SelectItem>
+        );
+    })
+
+    return(
+        <Select
+            onValueChange={(value:string) => {
+                handleSort(value);
+            }}
+            defaultValue={searchParams.get("sort")}
+        >
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Sort by" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+
+                    {sortable}
+
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+  }
