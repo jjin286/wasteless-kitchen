77b766505d0d2beab83a37cf64304743d8fa354c
JaeWon Jin <j1jin286@gmail.com>
2024-07-04 17:34:27 -0700

    Add ingredient page and database model WIP

--- app/components/CardSection.tsx
+++ app/components/CardSection.tsx
@@ -1,15 +1,5 @@
 import * as React from "react"
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-  import Image from "next/image";
-
-const IMAGE_BASE_URL = 'https://img.spoonacular.com/ingredients_100x100/';
+import FoodCard from "./FoodCard";
 
 export default function CardSection(props: {searchResult : Array<{
     id: number;
@@ -17,33 +7,18 @@ export default function CardSection(props: {searchResult : Array<{
     image: string;
     }>})
 {
+    function addIngredient(){
+
+    }
+
     const ingredients = props.searchResult.map((ingredient) => {
-        const src = `${IMAGE_BASE_URL}${ingredient.image}`;
         return (
-        <Card className="h-full w-full aspect-square transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 border-green-500">
-            <CardHeader>
-                <CardTitle className="text-center">{ingredient.name}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <Image
-                    loader={() => src}
-                    src={src}
-                    width={100}
-                    height={100}
-                    alt={ingredient.name}
-                    className="max-w-[100px] max-h-[100px] aspect-auto m-auto"
-
-                />
-            </CardContent>
-            <CardFooter>
-
-            </CardFooter>
-        </Card>
+            <FoodCard ingredient={ingredient}/>
         );
     })
 
     return(
-        <div className="grid grid-cols-4 gap-2 ">
+        <div className="grid grid-cols-3 gap-2 ">
             {props.searchResult
             ? ingredients
             : <p>Search for ingredients!</p>
--- app/components/FoodCard.tsx
+++ app/components/FoodCard.tsx
@@ -7,27 +7,82 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card";
-  import Image from "next/image";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+    SelectGroup,
+    SelectLabel,
+} from "@/components/ui/select";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CirclePlus, Plus, Minus } from "lucide-react"
 
-  const IMAGE_BASE_URL = 'https://img.spoonacular.com/ingredients_100x100/';
+const IMAGE_BASE_URL = 'https://img.spoonacular.com/ingredients_100x100/';
+const UNITS = [
+    'tsp', 'tbsp', 'c', 'pt', 'qt', 'gal', 
+    'oz', 'lb', 'mL', 'L', 'g', 'kg'
+]
 
-export default function FoodCard(){
+export default function FoodCard(props: {ingredient : {
+    id: number;
+    name: string;
+    image: string;
+    }}
+){
+    const src = `${IMAGE_BASE_URL}${props.ingredient.image}`;
+    const unitOption = UNITS.map((unit) => {
+        return(
+            <SelectItem
+                value={`${unit}`}
+                key={unit}
+            >
+                    {unit.charAt(0).toUpperCase() + unit.slice(1)}
+            </SelectItem>
+        );
+    })
+
     return(
-        <div>
-            <Card className="card p-12 m-5 bg-[#daffd6] rounded content-center">
-            <div className="image">
+        <Card className="h-full w-full aspect-square transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 border-green-500">
+            <CardHeader>
+                <CirclePlus />
+                <CardTitle className="text-center">{props.ingredient.name}</CardTitle>
+            </CardHeader>
+            <CardContent>
                 <Image
-                    src={IMAGE_BASE_URL + props.searchResult.image}
+                    loader={() => src}
+                    src={src}
                     width={100}
                     height={100}
-                    alt={text}
-                    className="mx-auto"
+                    alt={props.ingredient.name}
+                    className="max-w-[100px] max-h-[100px] aspect-auto m-auto"
+
                 />
-            </div>
-            <div className="text pt-5 font-medium">
-                <p></p>
-            </div>
+            </CardContent>
+            <CardFooter>
+                {/* <Button variant="outline" size="icon">
+                    <Minus className="h-4 w-4"/>    
+                </Button> */}
+                <Input type="number"/>
+                {/* <Button variant="outline" size="icon">
+                    <Plus className="h-4 w-4"/>
+                </Button> */}
+                <Select defaultValue={UNITS[0]}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Units" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+
+                            {unitOption}
+
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </CardFooter>
         </Card>
-        </div>
     );
 }
