a3b0ade04d302cf8dd244cf1c9a324457088b268
JaeWon Jin <j1jin286@gmail.com>
2024-09-16 20:42:39 -0700

    Pagination algorithm for ellipsis, testing required

--- app/components/RecipeCard.tsx
+++ app/components/RecipeCard.tsx
@@ -9,24 +9,25 @@ export default function RecipeCard(props: {recipe : any, handleAdd : any, delete
     let recipe = props.recipe.metadata ? props.recipe.metadata : props.recipe;
     console.log("Recipe", recipe, props.ingredients)
 
-    const missing = recipe.extendedIngredients.map((ingredient) => {
-        let included = false;
-        for (let i in props.ingredients){
-            if(i.id === ingredient.id)
-                included = true;
-        }
+    //Figure out if missing counter should be included
+    // const missing = recipe.extendedIngredients.map((ingredient) => {
+    //     let included = false;
+    //     for (let i in props.ingredients){
+    //         if(i.id === ingredient.id)
+    //             included = true;
+    //     }
 
-        if(!included) return ingredient.id;
-    })
-    console.log(missing)
+    //     if(!included) return ingredient.id;
+    // })
+    // console.log(missing)
 
     return(
     <div className="recipe-card flex flex-col bg-green-300 m-4 sm:w-2/5 w-1/5 h-2/4 rounded overflow-hidden relative">
         {handleAdd
-        ?<button id={recipe.id} className="absolute bottom-0 right-0 z-20 bg-green-300 rounded-full hover:text-white" onClick={handleAdd}>
-            <CirclePlus size={48} />
-        </button>
-        : null
+            ?<button id={recipe.id} className="absolute bottom-0 right-0 z-20 bg-green-300 rounded-full hover:text-white" onClick={handleAdd}>
+                <CirclePlus size={48} />
+            </button>
+            : null
         }
 
         <div className="relative h-1/2">
@@ -41,11 +42,11 @@ export default function RecipeCard(props: {recipe : any, handleAdd : any, delete
                     className=""
                     />
             </Link>
-            </div>
-            <div className="flex flex-col p-5">
+        </div>
+        <div className="flex flex-col p-5">
             <Link href={`/recipes/${recipe.id}`}>
                 <p><b>{recipe.title}</b></p>
-                    <p>Missing {missing.length}</p>
+                    {/* <p>Missing {missing.length}</p> */}
             </Link>
             <div className="h-1/4 absolute bottom-0">
                 <p className="flex"><AlarmClock /> &nbsp; {recipe.readyInMinutes ? recipe.readyInMinutes + ' minutes' : 'N/A'} </p>
