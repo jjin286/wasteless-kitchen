a82cc1bb612dd172c24b0ce811374678662fe6e3
JaeWon Jin <j1jin286@gmail.com>
2024-09-17 19:21:30 -0700

    Pagination ellipsis helper function working as intended, may change if ellipsis should be included in return array

--- utils/helper.ts
+++ utils/helper.ts
@@ -4,19 +4,20 @@
  * Returns an array of pages to be displayed on pagination
  */
 export default function paginationEllipsis(currPage: number, total : number, numDisplay : number){
-  let pageNumbers = Array.from(Array(total).keys());
+  let pageNumbers = Array.from(Array(total).keys()).slice(1);
   let left = Math.floor(numDisplay / 2);
   let right = numDisplay - left;
 
   //When numbers of pages exceed the total, show all pages
   if(numDisplay > total) return pageNumbers;
 
-  // When the current page is greater than the display count, add ellipsis and numbers around current page
-  if(currPage > numDisplay) return pageNumbers.slice(currPage - left, currPage + right);
+  // When the current page minus left is greater than 0, display numbers left and right of current page
+  if(currPage - left > 0) return pageNumbers.slice(currPage - left, currPage + right);
 
   // When the current page plus the numbers that should appear on the right, show all pages to end wwith ellipsis at start
   if(currPage + right > total) return pageNumbers.slice(-numDisplay);
 
   // When the current page minus the left is less than 0, show from first page to dispay count
-  if(currPage - left <= 0) return pageNumbers.slice(0, numDisplay);
+  if(currPage <= numDisplay) return pageNumbers.slice(0, numDisplay);
+  console.log("Trigger end");
 }
