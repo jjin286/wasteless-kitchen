85bbef982c64e47880bffc9738dbcd8060f61396
JaeWon Jin <j1jin286@gmail.com>
2024-09-23 16:46:07 -0700

    Added middleware for authentication, need to implement cookies to confirm user credentials

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const userToken = request.cookies.get('your-key')?.value;
+  // if(!userToken) {
+     return NextResponse.redirect(new URL('/login',request.url))
+  // }
+  // else {
+  //  return NextResponse.redirect(new URL('/', request.url))
+  // }
+}
+export const config = {
+  matcher: ['/recipes', '/ingredients/:path*'],
+}
