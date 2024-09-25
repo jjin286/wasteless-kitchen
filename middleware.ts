e57c8860b3c6ad8f48cab5d18446d80d18b7de8d
JaeWon Jin <j1jin286@gmail.com>
2024-09-25 15:28:11 -0700

    Middle ware for protected paths checking cookies

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,13 @@
 import { NextResponse } from "next/server";
 import type { NextFetchEvent, NextRequest } from "next/server";
-// import { createClient } from "@/utils/supabase/client";
-import { createClient } from "./utils/supabase/client";
-// import { createClient } from "@supabase/supabase-js";
 
 export function middleware(request: NextRequest, event: NextFetchEvent) {
-  console.log("Triggered middleware")
-  // const userToken = request.cookies.get('your-key')?.value;
-  const supabase = createClient();
-  let data = {};
+  const user = JSON.parse(request.cookies.get("sb-rpwcbpjlnwzwzsqxpigq-auth-token")?.value || null)?.user;
 
-  const checkLogin = async () => {
-    console.log("Triggered check login")
-    // data = await supabase.auth.getUser();
-    data = await supabase.auth.getSession();
-    console.log("Data inside check", data)
-  }
-
-  event.waitUntil(checkLogin());
-  // data = {user: {id : 123}}
-  console.log("Outside data", data)
-  if(data?.user?.id){
-    console.log("Inside valid user")
-    // return NextResponse.redirect(new URL('/', request.url))
+  if(user?.id){
     return NextResponse.next()
   }
   else {
-    console.log("Inside invlaid user")
     return NextResponse.redirect(new URL('/login',request.url))
   }
 }
