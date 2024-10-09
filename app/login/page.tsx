ecb115e7f2f8e5ed67846230e17f326879d0f74a
JaeWon Jin <j1jin286@gmail.com>
2024-10-09 13:29:09 -0700

    Added styling for OAuth buttons

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,8 @@
 import LoginForm from "../components/LoginForm";
 import Nav from "../components/Nav";
 import Link from "next/link";
-import { loginWithGoogle, loginWithFacebook, loginWithTwitter } from "@/app/api/oAuth/route"
+import { loginWithGoogle, loginWithFacebook, loginWithTwitter } from "@/app/api/oAuth/route";
+import Image from "next/image";
 
 export default function Login(){
 
@@ -30,17 +31,35 @@ export default function Login(){
                     <hr className="self-center w-3/5 border-black"/>
                 </div>
                 <div className="oauth-login">
-                    <div onClick={loginWithTwitter} className="twitter flex justify-center bg-white rounded p-3 my-3 border border-black">
-                        <p>Sign in with Twitter</p>
+                    <div onClick={loginWithTwitter} className="twitter flex justify-center bg-white rounded p-3 my-3 border border-black hover:bg-gray-50">
+                        <Image
+                            src={'/x-logo-black.png'}
+                            alt="Some picture of mine"
+                            width={24}
+                            height={24}
+                        />
+                        <p> Sign in with Twitter </p>
                     </div>
-                    <div onClick={loginWithGoogle} className="google flex justify-center bg-white rounded p-3 my-3 border border-black">
-                        <p>Sign in with Google</p>
+                    <div onClick={loginWithGoogle} className="google flex justify-center bg-white rounded p-3 my-3 border border-black hover:bg-gray-50">
+                        <Image
+                            src={'/google-logo.png'}
+                            alt="Some picture of mine"
+                            width={24}
+                            height={24}
+                        />
+                        <p> Sign in with Google</p>
                     </div>
-                    <div className="github flex justify-center bg-white rounded p-3 my-3 border border-black">
+                    {/* <div className="github flex justify-center bg-white rounded p-3 my-3 border border-black">
                         <p>Sign in with GitHub</p>
-                    </div>
-                    <div onClick={loginWithFacebook} className="apple flex justify-center bg-white rounded p-3 my-3 border border-black">
-                        <p>Sign in with Facebook</p>
+                    </div> */}
+                    <div onClick={loginWithFacebook} className="apple flex justify-center bg-white rounded p-3 my-3 border border-black hover:bg-gray-50">
+                        <Image
+                            src={'/Facebook_Logo_Primary.png'}
+                            alt="Some picture of mine"
+                            width={24}
+                            height={24}
+                        />
+                        <p> Sign in with Facebook</p>
                     </div>
                 </div>
             </div>
